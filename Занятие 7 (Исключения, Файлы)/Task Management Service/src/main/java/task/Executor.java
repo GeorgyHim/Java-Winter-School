e9b0f85264e0ce9db3014f9322e1b12039078a6e
georgyhim1997@yandex.ru <georgyhim1997>
2020-10-25 19:55:37 +0300

    Add ID to Executor

--- Занятие 7 (Исключения, Файлы)/Task Management Service/src/main/java/task/Executor.java
+++ Занятие 7 (Исключения, Файлы)/Task Management Service/src/main/java/task/Executor.java
@@ -3,10 +3,20 @@ package task;
 import java.time.LocalDate;
 
 public class Executor {
+    /** Общее количество исполнителей */
+    private static int count = 0;
+
+    /** Префикс для кода задачи*/
+    private static final String ID_PREFIX = "Executor-";
+
+    /** ID */
+    private String id;
+
     /** Имя */
     private String name;
 
     public Executor(String name) {
+        this.id = ID_PREFIX + count++;
         this.name = name;
     }
 }
