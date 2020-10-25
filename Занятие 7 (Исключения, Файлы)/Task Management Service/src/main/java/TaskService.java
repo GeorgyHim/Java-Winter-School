4b322d3aa772ac42d2b6ae3cda835b4ad8e310a1
georgyhim1997@yandex.ru <georgyhim1997>
2020-10-25 17:51:41 +0300

    Add TODO and initial collections

--- Занятие 7 (Исключения, Файлы)/Task Management Service/src/main/java/TaskService.java
+++ Занятие 7 (Исключения, Файлы)/Task Management Service/src/main/java/TaskService.java
@@ -1,7 +1,29 @@
-import java.util.HashSet;
-import java.util.Set;
+import task.Task;
+
+import java.util.*;
 
 public class TaskService {
-    /** Список задач */
-    Set<Task> tasks = new HashSet<>();
+    private static List<String> commands = Arrays.asList("help", "list", "add", "save <code>");
+
+    /** Список текущих задач */
+    private Set<Task> tasks = new HashSet<>();
+
+    public void process() {
+        Scanner in = new Scanner(System.in);
+    }
+
+    /*
+    TODO:
+    Функционал:
+        Вывести список доступных команд (отдельный enum?)
+        Прочитать и обработать команду (если такой команды нет, выбросить исключение)
+        В Мэйне выводить "Type HELP to see available commands"
+
+    Команды:
+        Добавить задачу (все варианты в одном методе):
+                (название)
+                (название + исполнитель)
+                (название + исполнитель + описание)
+        Сохранить задачу в память
+     */
 }
