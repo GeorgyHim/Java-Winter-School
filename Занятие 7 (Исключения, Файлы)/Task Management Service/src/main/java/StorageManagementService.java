9b5bcefdd461da5242e59ffb003a5e49614738d5
georgyhim1997@yandex.ru <georgyhim1997>
2020-10-27 21:45:58 +0300

    Refactoring StorageManagementService

--- Занятие 7 (Исключения, Файлы)/Task Management Service/src/main/java/StorageManagementService.java
+++ Занятие 7 (Исключения, Файлы)/Task Management Service/src/main/java/StorageManagementService.java
@@ -13,42 +13,43 @@ class StorageManagementService {
     /** Путь до глобальной папки хранения объектов*/
     private Path path;
 
-    /** Название подпапки для задач*/
-    private static String tasksFolder = "tasks";
-
-    /** Название подпапки для исполнителей*/
-    private static String executorsFolder = "executors";
-
     public StorageManagementService(String path) {
         this.path = Paths.get(path);
-        String sep = this.path.getFileSystem().getSeparator();
         try {
-            Files.createDirectories(Paths.get(this.path.toAbsolutePath().toString() + sep + tasksFolder + sep));
-            Files.createDirectories(Paths.get(this.path.toAbsolutePath().toString() + sep + executorsFolder + sep));
+            Files.createDirectories(Paths.get(getTasksFolder()));
+            Files.createDirectories(Paths.get(getExecutorsFolder()));
         } catch (IOException ignored) {}
+    }
+
+    private String getTasksFolder() {
+        String sep = this.path.getFileSystem().getSeparator();
+        return this.path.toAbsolutePath().toString() + sep + "tasks"  + sep;
+    }
+
+    private String getExecutorsFolder() {
+        String sep = this.path.getFileSystem().getSeparator();
+        return this.path.toAbsolutePath().toString() + sep + "executors"  + sep;
     }
 
     /** Получение файла объекта */
     private File getFile(String folder, String id) {
-        String sep = this.path.getFileSystem().getSeparator();
-        String path = this.path.toAbsolutePath().toString() + sep + folder + sep + id;
-        return new File(path);
+        return new File(folder + id);
     }
 
-    /** Созранение объекта */
+    /** Сохранение объекта */
     public void saveObject(Serializable object) throws IOException {
         FileOutputStream fileOutputStream = null;
         if (object instanceof Task)
-            fileOutputStream = new FileOutputStream(getFile(tasksFolder, ((Task) object).getId()));
+            fileOutputStream = new FileOutputStream(getFile(getTasksFolder(), ((Task) object).getId()));
         if (object instanceof Executor)
-            fileOutputStream = new FileOutputStream(getFile(executorsFolder, ((Executor) object).getId()));
+            fileOutputStream = new FileOutputStream(getFile(getExecutorsFolder(), ((Executor) object).getId()));
         try (ObjectOutputStream serializer = new ObjectOutputStream(fileOutputStream)) {
             serializer.writeObject(object);
         }
     }
 
     Executor findExecutor(String executor_id) throws IOException, ClassNotFoundException {
-        FileInputStream inputStream = new FileInputStream(getFile(executorsFolder, executor_id));
+        FileInputStream inputStream = new FileInputStream(getFile(getExecutorsFolder(), executor_id));
         try (ObjectInputStream serializer = new ObjectInputStream(inputStream)) {
             return (Executor) serializer.readObject();
         }
