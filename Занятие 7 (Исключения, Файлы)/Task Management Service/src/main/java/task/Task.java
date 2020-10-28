517c96a83bc52d0e80596d066f1a674578f25fa4
georgyhim1997@yandex.ru <georgyhim1997>
2020-10-28 19:42:29 +0300

    Delete unused setters

--- Занятие 7 (Исключения, Файлы)/Task Management Service/src/main/java/task/Task.java
+++ Занятие 7 (Исключения, Файлы)/Task Management Service/src/main/java/task/Task.java
@@ -58,24 +58,12 @@ public class Task implements Serializable {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public Executor getExecutor() {
--- Занятие 7 (Исключения, Файлы)/Task Management Service/src/test/java/TaskManagenentServiceTest.java
+++ Занятие 7 (Исключения, Файлы)/Task Management Service/src/test/java/TaskManagenentServiceTest.java
@@ -1,4 +1,4 @@
 public class TaskManagenentServiceTest {
-    // TODO: метод getFlag, manage, processSome, флаги, Неправильные команды
+    // TODO: метод getParams, manage, processSome, флаги, Неправильные команды
     // TODO: Тест updateData не учитывает файлы **Count
 }
