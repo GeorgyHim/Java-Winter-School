96de07942ef2746bf421ffde72f9ce3985d315e2
georgyhim1997@yandex.ru <georgyhim1997>
2020-10-28 16:21:06 +0300

    Write ExecutorTest

--- Занятие 7 (Исключения, Файлы)/Task Management Service/src/test/java/executor/ExecutorTest.java
+++ Занятие 7 (Исключения, Файлы)/Task Management Service/src/test/java/executor/ExecutorTest.java
@@ -1,4 +1,19 @@
 package executor;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ExecutorTest {
+    @Test
+    public void testCounting() {
+        Executor ex1 = new Executor("Goga");
+        Assertions.assertEquals("Executor-1", ex1.getId());
+        Assertions.assertEquals("Goga", ex1.getName());
+
+        Executor ex2 = new Executor("Alex");
+        Assertions.assertEquals("Executor-2", ex2.getId());
+        Assertions.assertEquals("Alex", ex2.getName());
+
+        Assertions.assertEquals(2, Executor.getCount());
+    }
 }
