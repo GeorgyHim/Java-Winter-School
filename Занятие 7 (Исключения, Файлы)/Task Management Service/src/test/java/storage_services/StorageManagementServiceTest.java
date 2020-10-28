8524a6c5ac46f280a84fbd725dffd26d4ed2a898
georgyhim1997@yandex.ru <georgyhim1997>
2020-10-28 21:32:47 +0300

    Maven works with another tests

--- Занятие 7 (Исключения, Файлы)/Task Management Service/src/test/java/storage_services/StorageManagementServiceTest.java
+++ Занятие 7 (Исключения, Файлы)/Task Management Service/src/test/java/storage_services/StorageManagementServiceTest.java
@@ -1,49 +1,50 @@
-//package storage_services;
-//
-//import executor.Executor;
-//import org.junit.jupiter.api.*;
-//import task.Task;
-//
-//import java.io.File;
-//import java.io.IOException;
-//
-//public class StorageManagementServiceTest {
-//
-//    @BeforeEach
-//    public void flushCounts() {
-//        CountSaver emptyCountSaver = new CountSaver("Folder1", "Folder2");
-//        Executor.loadCount(emptyCountSaver);
-//        Task.loadCount(emptyCountSaver);
-//    }
-//
-//    static String storagePath = System.getProperty("user.dir");
-//    private static StorageManagementService storageService = new StorageManagementService(storagePath);
-//
-//    @Test
-//    public void testSaveAndFindTask() throws IOException, ClassNotFoundException {
-//        Task task = new Task("testSaveTaskProblem");
-//        storageService.saveObject(task);
-//        File taskFile = new File(storageService.getTasksFolder() + task.getId());
-//        Task taskRead = storageService.findTask(task.getId());
-//        Assertions.assertEquals(task, taskRead);
-//        taskFile.delete();
-//    }
-//
-//    @Test
-//    public void testSaveAndFindExecutor() throws IOException, ClassNotFoundException {
-//        Executor executor = new Executor("testSaveExecutor");
-//        storageService.saveObject(executor);
-//        File execFile = new File(storageService.getExecutorsFolder() + executor.getId());
-//        Executor execRead = storageService.findExecutor(executor.getId());
-//        Assertions.assertEquals(executor, execRead);
-//        execFile.delete();
-//    }
-//
-//    @AfterAll
-//    static void clear() {
-//        File folder = new File(storageService.getTasksFolder());
-//        folder.delete();
-//        folder = new File(storageService.getExecutorsFolder());
-//        folder.delete();
-//    }
-//}
+package storage_services;
+
+import executor.Executor;
+import org.junit.jupiter.api.*;
+import task.Task;
+
+import java.io.File;
+import java.io.IOException;
+
+public class StorageManagementServiceTest {
+
+    @AfterEach
+    public void flushCounts() {
+        CountSaver emptyCountSaver = new CountSaver("Folder1", "Folder2");
+        Executor.loadCount(emptyCountSaver);
+        Task.loadCount(emptyCountSaver);
+    }
+
+    static String storagePath = System.getProperty("user.dir");
+    private static StorageManagementService storageService = new StorageManagementService(storagePath);
+
+    @Test
+    public void testSaveAndFindTask() throws IOException, ClassNotFoundException {
+        Task task = new Task("testSaveTaskProblem");
+        storageService.saveObject(task);
+        File taskFile = new File(storageService.getTasksFolder() + task.getId());
+        Task taskRead = storageService.findTask(task.getId());
+        Assertions.assertEquals(task, taskRead);
+        flushCounts();
+        taskFile.delete();
+    }
+
+    @Test
+    public void testSaveAndFindExecutor() throws IOException, ClassNotFoundException {
+        Executor executor = new Executor("testSaveExecutor");
+        storageService.saveObject(executor);
+        File execFile = new File(storageService.getExecutorsFolder() + executor.getId());
+        Executor execRead = storageService.findExecutor(executor.getId());
+        Assertions.assertEquals(executor, execRead);
+        execFile.delete();
+    }
+
+    @AfterAll
+    static void clear() {
+        File folder = new File(storageService.getTasksFolder());
+        folder.delete();
+        folder = new File(storageService.getExecutorsFolder());
+        folder.delete();
+    }
+}
