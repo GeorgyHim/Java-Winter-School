66ea3229448f2fc24b2ef0718775e41e03ec7617
georgyhim1997@yandex.ru <georgyhim1997>
2020-10-28 17:39:28 +0300

    Test bad counts

--- Занятие 7 (Исключения, Файлы)/Task Management Service/src/test/java/storage_services/CountSaverTest.java
+++ Занятие 7 (Исключения, Файлы)/Task Management Service/src/test/java/storage_services/CountSaverTest.java
@@ -1,7 +1,35 @@
 package storage_services;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
 public class CountSaverTest {
     // TODO: Тест когда файлов нет, должен возвращать 0
     // Тест когда файлы есть
     // Тест на запись
+
+    @Test
+    public void testGetCountsWithoutFiles() {
+        CountSaver countSaver = new CountSaver("Folder1", "Folder2");
+        Assertions.assertEquals(0, countSaver.getTaskCount());
+        Assertions.assertEquals(0, countSaver.getExecutorCount());
+    }
+
+    @Test
+    public void testGetCountsFromEmptyFiles() throws IOException {
+        CountSaver countSaver = new CountSaver(System.getProperty("user.dir") + "\\", System.getProperty("user.dir") + "\\");
+
+        File file = new File("TasksCount");
+        file.createNewFile();
+        Assertions.assertEquals(0, countSaver.getTaskCount());
+        file.delete();
+
+        file = new File("ExecutorsCount");
+        file.createNewFile();
+        Assertions.assertEquals(0, countSaver.getExecutorCount());
+        file.delete();
+    }
 }
