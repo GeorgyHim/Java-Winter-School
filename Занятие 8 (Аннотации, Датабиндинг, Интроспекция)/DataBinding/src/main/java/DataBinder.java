52e3c81e213d5a03167a55e996d87514469200b3
georgyhim1997@yandex.ru <georgyhim1997>
2020-10-31 20:03:11 +0300

    Write toXml

--- Занятие 8 (Аннотации, Датабиндинг, Интроспекция)/DataBinding/src/main/java/DataBinder.java
+++ Занятие 8 (Аннотации, Датабиндинг, Интроспекция)/DataBinding/src/main/java/DataBinder.java
@@ -1,2 +1,25 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
+
 public class DataBinder {
+    SerializationFeature feature;
+
+    public DataBinder(SerializationFeature feature) {
+        this.feature = feature;
+    }
+
+    private XmlMapper createXmlMapper() {
+        final XmlMapper mapper = new XmlMapper();
+        JaxbAnnotationModule module = new JaxbAnnotationModule();
+        mapper.registerModule(module);
+        mapper.enable(feature);
+        return mapper;
+    }
+
+    public String toXml(Object obj) throws JsonProcessingException {
+        XmlMapper mapper = createXmlMapper();
+        return mapper.writeValueAsString(obj);
+    }
 }
