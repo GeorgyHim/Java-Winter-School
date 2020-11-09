e84090d4357d3fc4917757a947e152a9dbcb9a71
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-09 18:44:08 +0300

    Using dataSource instead of DataSourceProvider

--- Занятие 9 (JDBC)/DataAccessObject/src/test/java/repository/MovieRepositoryTest.java
+++ Занятие 9 (JDBC)/DataAccessObject/src/test/java/repository/MovieRepositoryTest.java
@@ -2,30 +2,31 @@ package repository;
 
 import db.DataSourceProvider;
 import model.Movie;
+import org.apache.derby.jdbc.EmbeddedDataSource;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
 public class MovieRepositoryTest {
     static MovieRepository movieRepository;
-    static DataSourceProvider dataSourceProvider = null;
+    static EmbeddedDataSource dataSource = null;
     static Movie inception = new Movie("Inception", 150);
     static Movie leon = new Movie("Leon", 120);
 
     @BeforeAll
     static void init() throws IOException {
         try {
-            dataSourceProvider = new DataSourceProvider("test");
+            DataSourceProvider dataSourceProvider = new DataSourceProvider("test");
+            dataSource = dataSourceProvider.getDataSource();
         } catch (IOException e) {
             System.out.println("Ошибка создания провайдера: " + e.getMessage());
             throw e;
         }
-        movieRepository = new MovieRepository(dataSourceProvider.getDataSource());
+        movieRepository = new MovieRepository(dataSource);
     }
 
     @Test
@@ -53,7 +54,7 @@ public class MovieRepositoryTest {
 
     @AfterEach
     public void clearTable() throws SQLException {
-        try (Connection connection = dataSourceProvider.getDataSource().getConnection();
+        try (Connection connection = dataSource.getConnection();
              Statement statement = connection.createStatement()) {
 
             statement.executeUpdate("TRUNCATE TABLE " + MovieRepository.TABLE_NAME);
@@ -62,7 +63,7 @@ public class MovieRepositoryTest {
 
     @AfterAll
     static void shutdown() throws SQLException {
-        try (Connection connection = dataSourceProvider.getDataSource().getConnection();
+        try (Connection connection = dataSource.getConnection();
              Statement statement = connection.createStatement()) {
 
             statement.executeUpdate("DROP TABLE " + MovieRepository.TABLE_NAME);
