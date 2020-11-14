b2933ece3d0dcc4cf0dff30923ab80789be02de5
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-14 21:08:51 +0300

    Add constructors

--- Занятие 10 (Потоки)/ThreadMaxFinding/src/main/java/MaxFinder.java
+++ Занятие 10 (Потоки)/ThreadMaxFinding/src/main/java/MaxFinder.java
@@ -1,3 +1,52 @@
+import java.util.Scanner;
+
 public class MaxFinder {
-    // TODO: В первом задании вынести synchronized в сам метод
+    /** Число элементов в массиве */
+    private int number;
+
+    /** Количество потоков */
+    private int threadCount;
+
+    /** Массив чисел */
+    private int[] array;
+
+    public MaxFinder(int number, int threadCount, int[] array) {
+        this.number = number;
+        this.threadCount = threadCount;
+        this.array = array;
+    }
+
+    public MaxFinder(int number, int threadCount) {
+        this.number = number;
+        this.threadCount = threadCount;
+        readArray();
+    }
+
+    /**
+     * Метод ввода массива
+     */
+    private void readArray() {
+        array = new int[number];
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < number; i++)
+            array[i] = in.nextInt();
+    }
+
+
+
+    public int getThreadCount() {
+        return threadCount;
+    }
+
+    public void setThreadCount(int threadCount) {
+        this.threadCount = threadCount;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
