229680fe3bafcecf995a6d6f5823e9993570cbe8
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-27 20:51:53 +0300

    Write and improve DataSourceProvider

--- Итоговая задача/src/main/java/db/DataSourceProvider.java
+++ Итоговая задача/src/main/java/db/DataSourceProvider.java
@@ -1,4 +1,66 @@
 package db;
 
+import org.apache.derby.jdbc.EmbeddedDataSource;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Класс-провайдер для доступа к БД (JavaDB)
+ */
 public class DataSourceProvider {
+
+    /** DataSource */
+    private EmbeddedDataSource dataSource;
+
+    /** Параметры конфигурации */
+    private Properties properties;
+
+    /**
+     * Конструктор
+     *
+     * @throws IOException  -   Ошибка инициализации
+     */
+    public DataSourceProvider() throws IOException {
+        loadProperties();
+        createDataSource();
+    }
+
+    /**
+     * Метод загрузки конфигурации
+     *
+     * @throws IOException - ошибка загрузки конфигурации
+     */
+    private void loadProperties() throws IOException {
+        properties = new Properties();
+        try {
+            properties.load(
+                    Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties")
+            );
+        }
+        catch (Exception e) {
+            System.out.println("Error occurred during loading properties");
+            throw e;
+        }
+    }
+
+    /**
+     * Метод создания DataSource и при необходимости - самой БД
+     */
+    private void createDataSource() {
+        if (dataSource != null)
+            return;
+
+        dataSource = new EmbeddedDataSource();
+        dataSource.setUser("");
+        dataSource.setPassword("");
+        dataSource.setDatabaseName(properties.getProperty("dbname"));
+        dataSource.setCreateDatabase("create");
+    }
+
+    public EmbeddedDataSource getDataSource() {
+        if (dataSource == null)
+            createDataSource();
+        return dataSource;
+    }
 }
