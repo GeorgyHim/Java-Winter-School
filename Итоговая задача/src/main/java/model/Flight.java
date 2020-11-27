0df53550ca1b6aa15bcc0541e89f72fee4f8b9b7
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-27 20:56:13 +0300

    Flight class initial commit

---
+++ Итоговая задача/src/main/java/model/Flight.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * Рейс
+ */
+public class Flight {
+
+    /** Название таблицы в БД */
+    public static final String TABLE_NAME = "flight";
+
+    // TODO: Создать нужные поля класса, а также дополнить их аннотациями для датабиндинга в XML
+}
