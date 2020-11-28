ea857274707f23415a5041957348a250c079d878
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-28 18:27:41 +0300

    Add fields to Flight class

--- Итоговая задача/src/main/java/model/Flight.java
+++ Итоговая задача/src/main/java/model/Flight.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.time.LocalDateTime;
+
 /**
  * Рейс
  */
@@ -7,6 +9,30 @@ public class Flight {
 
     /** Название таблицы в БД */
     public static final String TABLE_NAME = "flight";
+
+    /** Идентификатор */
+    private Integer id;
+
+    /** Номер рейса */
+    private String number;
+
+    /** Город вылета */
+    private String cityFrom;
+
+    /** Город прилета */
+    private String cityTo;
+
+    /** Авиакомпания */
+    private String airline;
+
+    /** Время вылета */
+    private LocalDateTime departureTime;
+
+    /** Время прилета */
+    private LocalDateTime arrivalTime;
+
+    /** Статус */
+    private FlightStatus status;
 
     // TODO: Создать нужные поля класса, а также дополнить их аннотациями для датабиндинга в XML
 }
