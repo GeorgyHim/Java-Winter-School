8846eb966f0d8f4e2512046efe267e525d256814
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-28 18:42:57 +0300

    Add constructors to Flight class

--- Итоговая задача/src/main/java/model/Flight.java
+++ Итоговая задача/src/main/java/model/Flight.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
  * Рейс
  */
 public class Flight {
+    // TODO: Создать нужные поля класса, а также дополнить их аннотациями для датабиндинга в XML
 
     /** Название таблицы в БД */
     public static final String TABLE_NAME = "flight";
@@ -34,5 +35,24 @@ public class Flight {
     /** Статус */
     private FlightStatus status;
 
-    // TODO: Создать нужные поля класса, а также дополнить их аннотациями для датабиндинга в XML
+    /** Конструктор для создания объектов в коде */
+    public Flight(String number, String cityFrom, String cityTo, String airline,
+                  LocalDateTime departureTime, LocalDateTime arrivalTime, FlightStatus status) {
+
+        this(null, number, cityFrom, cityTo, airline, departureTime, arrivalTime, status);
+    }
+
+    /** Конструктор для создания объектов при загрузке из базы */
+    public Flight(Integer id, String number, String cityFrom, String cityTo, String airline,
+                  LocalDateTime departureTime, LocalDateTime arrivalTime, FlightStatus status) {
+
+        this.id = id;
+        this.number = number;
+        this.cityFrom = cityFrom;
+        this.cityTo = cityTo;
+        this.airline = airline;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.status = status;
+    }
 }
