27faf5bc578672ce6beb8f1db488665b9e271c67
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-29 16:35:13 +0300

    Add empty constructor and equals method in FlightList class

--- Итоговая задача/src/main/java/utils/FlightList.java
+++ Итоговая задача/src/main/java/utils/FlightList.java
@@ -7,6 +7,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlList;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
+import java.util.Objects;
 
 @XmlRootElement(name = "flightList")
 public class FlightList {
@@ -18,7 +19,24 @@ public class FlightList {
         this.flights = flights;
     }
 
+    public FlightList() {
+    }
+
     public List<Flight> getFlights() {
         return flights;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FlightList that = (FlightList) o;
+
+        return flights.equals(that.getFlights());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(flights);
     }
 }
