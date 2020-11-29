b20edecaab7f9a3b9eddfd94f849b6510d3898fc
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-29 16:37:21 +0300

    Write XmlConverterTest

--- Итоговая задача/src/test/java/utils/XmlConverterTest.java
+++ Итоговая задача/src/test/java/utils/XmlConverterTest.java
@@ -1,15 +1,15 @@
 package utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import model.Flight;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
 public class XmlConverterTest {
-    static XmlConverter converter = new XmlConverter();
+    static XmlConverter converter = new XmlConverter(false);
     static Flight flight1 = new Flight(
             "US-228", "Краснодар", "Санкт-Петербург", "SomebodyAirlines",
             LocalDateTime.of(2020, 11, 28, 10, 0), LocalDateTime.of(2020, 11, 28, 12, 30), FlightStatus.EN_ROUTE
@@ -20,10 +20,20 @@ public class XmlConverterTest {
     );
 
     @Test
-    public void testSerializing() throws JsonProcessingException {
+    public void testSerialize() throws IOException {
         FlightList flightList = new FlightList(Arrays.asList(flight1, flight2));
         String xml = converter.toXml(flightList);
-        // TODO:
-        Assertions.assertTrue(true);
+        Assertions.assertEquals(
+                xml,
+                "<flightList><flight><cityFrom>Краснодар</cityFrom><cityTo>Санкт-Петербург</cityTo><departureDateTime>2020-11-28 10:00</departureDateTime><arrivalDateTime>2020-11-28 12:30</arrivalDateTime><airline>SomebodyAirlines</airline><number>US-228</number><status>EN_ROUTE</status></flight><flight><cityFrom>Сочи</cityFrom><cityTo>Москва</cityTo><departureDateTime>2020-11-30 15:00</departureDateTime><arrivalDateTime>2020-11-30 17:00</arrivalDateTime><airline>AwesomeFly</airline><number>TT-322</number><status>SCHEDULED</status></flight></flightList>"
+        );
+    }
+
+    @Test
+    public void testDeserialize() throws IOException {
+        FlightList flightList = new FlightList(Arrays.asList(flight1, flight2));
+        String xml = converter.toXml(flightList);
+        FlightList flightList2 = converter.fromXml(xml, FlightList.class);
+        Assertions.assertEquals(flightList, flightList2);
     }
 }
