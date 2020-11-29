f7ba9e4594dd231415876f9bafca6f5726564266
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-29 15:10:45 +0300

    Add javadocs to XmlConverter

--- Итоговая задача/src/main/java/utils/XmlConverter.java
+++ Итоговая задача/src/main/java/utils/XmlConverter.java
@@ -8,12 +8,18 @@ import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 import java.io.IOException;
 
 public class XmlConverter {
+    /** Преобразователь объектов в XML и обратно */
     private final XmlMapper mapper;
 
     public XmlConverter() {
         this.mapper = createXmlMapper();
     }
 
+    /**
+     * Метод создания XML-преобразователя
+     *
+     * @return  -   XML-преобразователь
+     */
     private XmlMapper createXmlMapper() {
         final XmlMapper mapper = new XmlMapper();
         JaxbAnnotationModule module = new JaxbAnnotationModule();
@@ -22,11 +28,27 @@ public class XmlConverter {
         return mapper;
     }
 
+    /**
+     * Метод сериализации объекта в XML
+     *
+     * @param obj                       -   Объект
+     * @return                          -   XML-строка сериализованного объекта
+     * @throws JsonProcessingException  -   Исключение при сериализации
+     */
     public String toXml(Object obj) throws JsonProcessingException {
         return mapper.writeValueAsString(obj);
     }
 
-    public <T> T fromXml(String xml, Class<T> type) throws IOException {
-        return mapper.readValue(xml, type);
+    /**
+     * Метод десериализации объекта из XML
+     *
+     * @param xml           -   Строка XML
+     * @param cls           -   Класс объекта
+     * @param <T>           -   Тип класса
+     * @return              -   Объект нужного класса
+     * @throws IOException  -   Исключение при десериализации объекта
+     */
+    public <T> T fromXml(String xml, Class<T> cls) throws IOException {
+        return mapper.readValue(xml, cls);
     }
 }
