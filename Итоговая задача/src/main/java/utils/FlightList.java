ca868a8fdfc31ffe263fd8edb533e4e9d6ca0fe8
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-29 15:02:16 +0300

    Create FlightList class for binding list of flights

--- Итоговая задача/src/main/java/utils/FlightList.java
+++ Итоговая задача/src/main/java/utils/FlightList.java
@@ -1,4 +1,21 @@
 package utils;
 
+import model.Flight;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "flightList")
 public class FlightList {
+    @XmlElement(name = "flight")
+    private List<Flight> flights;
+
+    public FlightList(List<Flight> flights) {
+        this.flights = flights;
+    }
+
+    public List<Flight> getFlights() {
+        return flights;
+    }
 }
